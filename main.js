cddd6f24eb8478411e0fa1ba42685acebe8217f2
Jimmy <hieunm06@wru.vn>
2017-08-16 17:18:37 +0700

    Update main.js
    Copy code from tampermonkey so users can just paste it.

--- main.js
+++ main.js
@@ -1,90 +1,102 @@
-// answers //summary answer
-// groups //4 rows answer
-// stopTheClock(); //no timer
+// ==UserScript==
+// @name         Only Connect cheat
+// @namespace    https://github.com/gixid192/only-connect-bbc-cheat/
+// @version      0.1
+// @description  Cheat the game
+// @author       Jimmy
+// @match        http://www.puzzgrid.com/grid/*
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    //only connect
+    // answers //summary answer
+    // groups //4 rows answer
+    // stopTheClock(); //no timer
 
-const menu = document.querySelector('#menu');
-const beginBtn = document.querySelector('#overlay');
+    'use strict';
 
-const appendCheatBtn = () => {
-    const cheatDiv = document.createElement('div');
+    const menu = document.querySelector('#menu');
+    const beginBtn = document.querySelector('#overlay');
 
-    menu.style.top = '50px';
-    cheatDiv.style.margin = "20px auto";
+    const appendCheatBtn = () => {
+        const cheatDiv = document.createElement('div');
 
-    menu.appendChild(cheatDiv);
+        menu.style.top = '50px';
+        cheatDiv.style.margin = "20px auto";
 
-    const noTimer = document.createElement('p');
-    const revealRow = document.createElement('p');
-    const revealSummary = document.createElement('p');
-    
+        menu.appendChild(cheatDiv);
 
-    noTimer.textContent = 'Stop Timer';
-    noTimer.id = 'noTimer';
+        const noTimer = document.createElement('p');
+        const revealRow = document.createElement('p');
+        const revealSummary = document.createElement('p');
 
-    revealRow.textContent = 'Reveal Row';
-    revealRow.id = 'revealRow';
+        noTimer.textContent = 'Stop Timer';
+        noTimer.id = 'noTimer';
 
-    revealSummary.textContent = 'Reveal Summary';
-    revealSummary.id = 'revealSummary';
-    
-    const styleBtn = "color: #deadbeef; margin-right: 20px; display: inline-block; font-weight: 600; cursor: pointer";
-    noTimer.style.cssText = styleBtn;
-    revealRow.style.cssText = styleBtn;
-    revealSummary.style.cssText = styleBtn;
+        revealRow.textContent = 'Reveal Row';
+        revealRow.id = 'revealRow';
 
-    cheatDiv.appendChild(noTimer);
-    cheatDiv.appendChild(revealRow);
-    cheatDiv.appendChild(revealSummary);
+        revealSummary.textContent = 'Reveal Summary';
+        revealSummary.id = 'revealSummary';
 
-}
-const revealRow = () => {
-    const colors = ["red", "blue", "green", "yellow"];
-    groups.map( (group,key) => {
-        group.map(number => {
-            elm = document.querySelector('#tile' + number);
-            elm.style.color = colors[key];
-        })
-    })
-}
+        const styleBtn = "color: #deadbeef; margin-right: 20px; display: inline-block; font-weight: 600; cursor: pointer";
+        noTimer.style.cssText = styleBtn;
+        revealRow.style.cssText = styleBtn;
+        revealSummary.style.cssText = styleBtn;
 
-const revealSummary = () => {
-    const ianswers = answers.map(arr => arr[0]);
-    groups.map( (group,key) => {
-        group.map(number => {
-            elm = document.querySelector('#tile' + number);
-            txt = elm.textContent;
-            elm.innerHTML = txt + '<br>' + ianswers[key];
-        })
-    })
-}
-const initListener = () => {
-    menu.addEventListener('click', function(e){
-        const target = e.target;
-        const elID = target.id;
-        if(target.getAttribute('clicked')){
-            return;
+        cheatDiv.appendChild(noTimer);
+        cheatDiv.appendChild(revealRow);
+        cheatDiv.appendChild(revealSummary);
+
+    };
+    const revealRow = () => {
+        const colors = ["red", "blue", "green", "yellow"];
+        groups.map( (group,key) => {
+            group.map(number => {
+                const elm = document.querySelector('#tile' + number);
+                elm.style.color = colors[key];
+            });
+        });
+    };
+
+    const revealSummary = () => {
+        const ianswers = answers.map(arr => arr[0]);
+        groups.map( (group,key) => {
+            group.map(number => {
+                const elm = document.querySelector('#tile' + number);
+                elm.innerHTML = elm.textContent + '<br>' + ianswers[key];
+            });
+        });
+    };
+    const initListener = () => {
+        menu.addEventListener('click', function(e){
+            const target = e.target;
+            const elID = target.id;
+            if(target.getAttribute('clicked')){
+                return;
+            }
+            target.setAttribute('clicked',1);
+            switch (elID){
+                case "noTimer":
+                    stopTheClock();
+                    break;
+                case "revealRow":
+                    revealRow();
+                    break;
+                case "revealSummary":
+                    revealSummary();
+                    break;
+                default: return;
+            }
+        });
+    };
+
+    const waitFor = setInterval(function(){
+        if(beginBtn.style.display){
+            clearInterval(waitFor);
+            appendCheatBtn();
+            initListener();
         }
-        target.setAttribute('clicked',1);
-        switch (elID){
-            case "noTimer":
-                stopTheClock();
-                break;
-            case "revealRow":
-                revealRow();
-                break;
-            case "revealSummary":
-                revealSummary();
-                break;
-            default: return;
-        }
-    })
-}
-
-const waitFor = setInterval(function(){
-    if(beginBtn.style.display){
-        clearInterval(waitFor);
-        appendCheatBtn();
-        initListener();
-    }
-}, 1000);
+    }, 1000);
+})();
